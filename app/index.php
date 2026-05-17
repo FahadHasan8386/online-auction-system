2056dbc1c4df5d67ad5c2c76a8daf2a4f65e82b0
tanvir <tanvirrahman2003123@gmail.com>
2026-05-17 19:12:58 +0600

    Add index and log out

--- app/index.php
+++ app/index.php
@@ -1,24 +1,15 @@
 <?php
-// Main index file
-// Location: OnlineAuctionSystem/index.php
-
 session_start();
 
 if (isset($_SESSION['user_id'])) {
-    if ($_SESSION['role'] == 'admin') {
-        header('Location: views/admin/adminDashboard.php');
-        exit();
-    } elseif ($_SESSION['role'] == 'buyer') {
-        header('Location: views/buyer/buyerdashboard.php');
-        exit();
+    if ($_SESSION['role'] == 'buyer') {
+        header("Location: views/buyer/dashboard.php");
     } elseif ($_SESSION['role'] == 'seller') {
-        header('Location: views/seller/sellerDashboard.php');
-        exit();
-    } elseif ($_SESSION['role'] == 'moderator') {
-        header('Location: views/moderator/moderatorDashboard.php');
-        exit();
+        header("Location: views/seller/dashboard.php");
+    } else {
+        header("Location: views/admin/dashboard.php");
     }
+} else {
+    header("Location: views/login.php");
 }
-
-header('Location: views/shared/login.php');
-exit();
+?>
